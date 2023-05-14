edcd90e3a75a21103d755f3be0f5f78cbd2e81cb
juchan2021 <60370842+juchan2021@users.noreply.github.com>
2023-05-14 02:29:19 -0700

    audio fudging works

--- src/tabs/MainTab.tsx
+++ src/tabs/MainTab.tsx
@@ -1,17 +1,101 @@
 //MainTab.tsx
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMicrophone,
+  faPause,
+  faPlay,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
+
 import ReactQuill from "react-quill";
+import { useReactMediaRecorder } from "react-media-recorder";
 import "react-quill/dist/quill.snow.css";
 import "../App.css";
+
+//const mimeType = "audio/webm";
 const MainTab = () => {
+  const {
+    status,
+    startRecording,
+    pauseRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ audio: true });
+  const download = () => {
+    if (mediaBlobUrl) {
+      var element = document.createElement("a");
+      element.setAttribute("href", mediaBlobUrl);
+      element.setAttribute("download", "audio.webm");
+      element.style.display = "none";
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    }
+  };
   const [showMainTab, setShowMainTab] = useState(false);
+  /*const [permission, setPermission] = useState(false);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+  const [audio, setAudio] = useState<string | null>(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+
+  const getMicPermission = async () => {
+    try {
+      const streamData = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      setPermission(true);
+      setStream(streamData);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert("Unknown error");
+      }
+    }
+  };
+
+  const startRecording = async () => {
+    setRecordingStatus("recording");
+    //create new Media recorder instance using the stream
+    if (stream) {
+      const media = new MediaRecorder(stream, { mimeType });
+      //set the MediaRecorder instance to the mediaRecorder ref
+      mediaRecorder.current = media;
+      //invokes the start method to start the recording process
+      mediaRecorder.current.start();
+      let localAudioChunks: Blob[] = [];
+      mediaRecorder.current.ondataavailable = (event) => {
+        if (typeof event.data === "undefined") return;
+        if (event.data.size === 0) return;
+        localAudioChunks.push(event.data);
+      };
+      setAudioChunks(localAudioChunks);
+    }
+  };
+  const stopRecording = () => {
+    setRecordingStatus("inactive");
+    //stops the recording instance
+    if (mediaRecorder.current) {
+      mediaRecorder.current.stop();
+      mediaRecorder.current.onstop = () => {
+        //creates a blob file from the audiochunks data
+        const audioBlob = new Blob(audioChunks, { type: mimeType });
+        //creates a playable URL from the blob file.
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudio(() => audioUrl);
+        setAudioChunks([]);
+      };
+    }
+  };*/
 
   const handleClick = () => {
     setShowMainTab(true);
   };
-  const [text, setText] = useState(""); console.log(text);
+  const [text, setText] = useState("");
+  console.log(text);
   const [paperOptions, setPaperOptions] = useState([
     { label: "Transcript", color: true },
     { label: "Notes", color: false },
@@ -34,7 +118,12 @@ const MainTab = () => {
 
       <div className="main-body-container">
         <div className="paper">
-            <ReactQuill value={text} onChange={setText} style={{color: "black"}} placeholder="Or paste your notes here" />
+          <ReactQuill
+            value={text}
+            onChange={setText}
+            style={{ color: "black" }}
+            placeholder="Or paste your notes here"
+          />
         </div>
         <div className="paper-options">
           {paperOptions.map((option, index) => (
@@ -51,12 +140,31 @@ const MainTab = () => {
             </button>
           ))}
 
-          <button className="record-button">
+          <button onClick={startRecording} className="record-button">
             <div className="mic-icon">
               <FontAwesomeIcon icon={faMicrophone} />
               <div className="mic-text">Record</div>
             </div>
           </button>
+
+          {status === "recording" && (
+            <button onClick={stopRecording} className="record-button">
+              <div className="mic-icon">
+                <FontAwesomeIcon icon={faPause} />
+                <div className="mic-text">Stop</div>
+              </div>
+            </button>
+          )}
+
+          {status === "stopping" ||
+            (status === "stopped" && (
+              <button onClick={download} className="record-button">
+                <div className="mic-icon">
+                  <FontAwesomeIcon icon={faDownload} />
+                  <div className="mic-text">Download</div>
+                </div>
+              </button>
+            ))}
         </div>
       </div>
       {showMainTab && <div>Main tab content</div>}
